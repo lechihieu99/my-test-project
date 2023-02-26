437d16c5249d1c8359263d1784912e502c5e741a
lechihieu99 <19110073@student.hcmus.edu.vn>
2023-02-26 14:20:52 +0700

    Updates

---
+++ precache-manifest.2fe1af130ddd8cafd1b03cfb883d344f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d7e8e285dd6e4777c0f",
+    "url": "/my-test-project/static/css/main.7bcafa6d.chunk.css"
+  },
+  {
+    "revision": "1d7e8e285dd6e4777c0f",
+    "url": "/my-test-project/static/js/main.1d7e8e28.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/my-test-project/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "c418a5d202bbcabde800",
+    "url": "/my-test-project/static/js/2.c418a5d2.chunk.js"
+  },
+  {
+    "revision": "db4c36485e90f036980d",
+    "url": "/my-test-project/static/js/runtime~main.db4c3648.js"
+  },
+  {
+    "revision": "ef359a4ec353f824d81223f3ed510ab4",
+    "url": "/my-test-project/static/media/Epilogue-Medium.ef359a4e.ttf"
+  },
+  {
+    "revision": "bb7e14657d41eaa0f322cf699bbc8cde",
+    "url": "/my-test-project/static/media/Epilogue-Black.bb7e1465.ttf"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-test-project/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "3c59ec892d4d448f4e313a57e33b31f7",
+    "url": "/my-test-project/static/media/pic1.3c59ec89.jpg"
+  },
+  {
+    "revision": "f4928ff024b9697247cb1996e737e586",
+    "url": "/my-test-project/static/media/pic2.f4928ff0.jpeg"
+  },
+  {
+    "revision": "cd1301d60d9fdac025917d36d9eb45b2",
+    "url": "/my-test-project/static/media/pic3.cd1301d6.jpg"
+  },
+  {
+    "revision": "6ff209126b14b160053afdbd05e965b0",
+    "url": "/my-test-project/static/media/pic6.6ff20912.jpeg"
+  },
+  {
+    "revision": "7fbba3c4dff902cb784fef86a1f287fb",
+    "url": "/my-test-project/static/media/pic7.7fbba3c4.jpg"
+  },
+  {
+    "revision": "d9151b2355e14172e08757e40da9506d",
+    "url": "/my-test-project/static/media/pic5.d9151b23.jpeg"
+  },
+  {
+    "revision": "99e99b5a91e4d07b9acfbb8c9d506190",
+    "url": "/my-test-project/static/media/pic8.99e99b5a.jpg"
+  },
+  {
+    "revision": "c5ced750b3fcc62e360bd559f1dc59f3",
+    "url": "/my-test-project/static/media/pic9.c5ced750.jpg"
+  },
+  {
+    "revision": "1442e085a9f0e908e48007588428751b",
+    "url": "/my-test-project/static/media/pic11.1442e085.jpeg"
+  },
+  {
+    "revision": "7fbba3c4dff902cb784fef86a1f287fb",
+    "url": "/my-test-project/static/media/pic12.7fbba3c4.jpg"
+  },
+  {
+    "revision": "bdc654a6e7ab8ba9796876be00fd759d",
+    "url": "/my-test-project/static/media/pic13.bdc654a6.jpg"
+  },
+  {
+    "revision": "3347282bf3c302af4579c723d51e768c",
+    "url": "/my-test-project/static/media/pic15.3347282b.jpg"
+  },
+  {
+    "revision": "7c03e2b4751eef2e82a49fabc545925f",
+    "url": "/my-test-project/static/media/pic10.7c03e2b4.jpeg"
+  },
+  {
+    "revision": "b4630fcb5f7ec496b6a7bdeadea7c4a4",
+    "url": "/my-test-project/static/media/pic14.b4630fcb.jpg"
+  },
+  {
+    "revision": "a1dfb39ea978505d366152ac00c6885e",
+    "url": "/my-test-project/static/media/pic16.a1dfb39e.jpg"
+  },
+  {
+    "revision": "f811f5ed65ed5050ef24bf70bfcff17b",
+    "url": "/my-test-project/static/media/pic17.f811f5ed.jpg"
+  },
+  {
+    "revision": "5950816eb394369178b3674ccadf4aa1",
+    "url": "/my-test-project/static/media/pic4.5950816e.jpeg"
+  },
+  {
+    "revision": "76cb2ed90fd3557e524c63068069cdb0",
+    "url": "/my-test-project/index.html"
+  }
+];
