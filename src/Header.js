375753faa9eb129a079af50565567e734a341258
lechihieu99 <19110073@student.hcmus.edu.vn>
2023-03-01 15:04:42 +0700

    Deployed website

--- src/Header.js
+++ src/Header.js
@@ -7,10 +7,11 @@ export default class Header extends Component {
         return(
             <>
             <div id="header">
-                <MainHeader  number='+1 (123) 456 78 90' email='mobirise@company.com'/>
+                <MainHeader  number='+84 339 042 674' email='19110073@student.hcmus.edu.vn'/>
                 
             </div>
             <Menu />
+            <div id="note">Website chỉ là bản test nhiều thiếu sót. Page load hơi chậm, vui lòng thông cảm!</div>
             </>
             
         );
